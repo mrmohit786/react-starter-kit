d3bdb732188ff7f2e89d71438c5150427f31ea0c
Mohit <patelmohit719@gmail.com>
2022-10-19 22:44:38 +0530

    test

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ const App = lazy(() => import('./App'));
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-	<Suspense fallback={<div>Loading...</div>}>
+	<Suspense fallback={<div>Loading....</div>}>
 		<App />
 	</Suspense>,
 );
